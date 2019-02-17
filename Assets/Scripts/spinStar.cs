5f3aa641a7cf9255621220c503acdda3f78a009c
cmkane <carissakane@dhcp-v092-225.mobile.uci.edu>
2019-02-16 16:48:26 -0800

    Add main menu with start button

---
+++ Assets/Scripts/spinStar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class spinStar : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime);
+    }
+}
